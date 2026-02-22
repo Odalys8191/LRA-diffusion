19fec5111c3abe22f2350ed47f536c8ed768a5f3
Odalys <38152534+Odalys8191@users.noreply.github.com>
2026-02-23 00:44:37 +0800

    Replace PyTorch with Jittor in test script

--- test_ILSVRC2012.py
+++ test_ILSVRC2012.py
@@ -1,41 +1,38 @@
 import numpy as np
-import torch.utils.data as data
+import jittor.dataset as data
 from tqdm import tqdm
 from utils.ema import EMA
 from utils.clip_wrapper import clip_img_wrap
 from utils.ILSVRC2012_data_utils import ILSVRC2012
-import torch.optim as optim
+import jittor.optim as optim
 from utils.learning import *
 from model_diffusion import Diffusion
 from utils.knn_utils import sample_knn_labels, knn, knn_labels, prepare_knn
-import torch.multiprocessing
-torch.multiprocessing.set_sharing_strategy('file_system')
 import argparse
-torch.manual_seed(123)
-torch.cuda.manual_seed(123)
+import random
+jittor.manual_seed(123)
 np.random.seed(123)
 random.seed(123)
 
 
 def test(diffusion_model, test_loader, test_embed):
 
-    if not torch.is_tensor(test_embed):
-        test_embed = torch.tensor(test_embed).to(torch.float)
+    if not isinstance(test_embed, jittor.Var):
+        test_embed = jittor.array(test_embed).float32()
 
     correct_cnt = 0
-    with torch.no_grad():
+    with jittor.no_grad():
         diffusion_model.model.eval()
         diffusion_model.diffusion_encoder.eval()
         diffusion_model.fp_encoder.eval()
         for test_batch_idx, data_batch in tqdm(enumerate(test_loader), total=len(test_loader), desc=f'evaluating diff', ncols=100):
             [x_batch, target, indicies] = data_batch[:3]
-            target = target.to(device)
-            fp_embed = test_embed[indicies, :].to(device)
-            label_t_0 = diffusion_model.reverse_ddim(x_batch, stochastic=False, fp_x=fp_embed).detach().cpu()
-            # acc_temp = accuracy(label_t_0.detach().cpu(), target.cpu())[0].item()
+            fp_embed = test_embed[indicies, :]
+            label_t_0 = diffusion_model.reverse_ddim(x_batch, stochastic=False, fp_x=fp_embed).detach()
+            # acc_temp = accuracy(label_t_0.detach(), target)[0].item()
             # acc_avg += acc_temp
 
-            correct = cnt_agree(label_t_0.detach().cpu(), target.cpu())
+            correct = cnt_agree(label_t_0.detach(), target)
             correct_cnt += correct
 
     acc = 100 * correct_cnt / test_embed.shape[0]
@@ -60,7 +57,7 @@ if __name__ == "__main__":
     if args.device is None:
         gpu_devices = ','.join([str(id) for id in args.gpu_devices])
         os.environ["CUDA_VISIBLE_DEVICES"] = gpu_devices
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        device = 'cuda' if jittor.has_cuda else 'cpu'
     else:
         device = args.device
 
@@ -70,7 +67,7 @@ if __name__ == "__main__":
     ILSVRC2012_dir = os.path.join(os.getcwd(), 'ILSVRC2012')
     print('data_dir', ILSVRC2012_dir)
     val_dataset = ILSVRC2012(data_root=ILSVRC2012_dir)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=512, shuffle=False, num_workers=args.num_workers)
+    val_loader = data.DataLoader(val_dataset, batch_size=512, shuffle=False, num_workers=args.num_workers)
 
     # initialize diffusion model
     fp_encoder = clip_img_wrap('ViT-L/14', device, center=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
@@ -79,19 +76,18 @@ if __name__ == "__main__":
     diffusion_model = Diffusion(fp_encoder, num_timesteps=1000, n_class=n_class, fp_dim=fp_dim, device=device,
                                 feature_dim=args.feature_dim, encoder_type=args.diff_encoder,
                                 ddim_num_steps=args.ddim_n_step, beta_schedule='cosine')
-    state_dict = torch.load(model_path, map_location=torch.device(device))
-    diffusion_model.load_diffusion_net(state_dict)
+    # state_dict = jittor.load(model_path)
+    # diffusion_model.load_diffusion_net(state_dict)
     diffusion_model.fp_encoder.eval()
 
     # DataParallel wrapper
     if args.device is None:
         print('using DataParallel')
-        diffusion_model.model = nn.DataParallel(diffusion_model.model).to(device)
-        diffusion_model.diffusion_encoder = nn.DataParallel(diffusion_model.diffusion_encoder).to(device)
-        diffusion_model.fp_encoder = nn.DataParallel(fp_encoder).to(device)
+        diffusion_model.model = nn.DataParallel(diffusion_model.model)
+        diffusion_model.diffusion_encoder = nn.DataParallel(diffusion_model.diffusion_encoder)
+        diffusion_model.fp_encoder = nn.DataParallel(fp_encoder)
     else:
         print('using single gpu')
-        diffusion_model.to(device)
 
     print('pre-computing fp embeddings for validation data for ILSVRC2012')
     val_embed_dir = os.path.join(ILSVRC2012_dir, f'fp_embed_val_ILSVRC2012.npy')
